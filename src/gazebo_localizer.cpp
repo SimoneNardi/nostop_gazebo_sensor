af7e0bd1747d7c68b8b0986078f43f93cee4c798
Simone Nardi <monena@tiscali.it>
2015-10-29 22:46:48 +0100

    Modificato nome del topic

--- src/gazebo_localizer.cpp
+++ src/gazebo_localizer.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
       ///
       std::string l_pub_name = "/";
       l_pub_name += l_name;
-      l_pub_name += "/gazebo/localizer";
+      l_pub_name += "/localizer/gazebo/pose";
       
       ros::Publisher l_posePub = l_node.advertise<geometry_msgs::Pose>(l_pub_name.c_str(), 10);
       ros::ServiceClient l_clientGet = l_node.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state"); 
